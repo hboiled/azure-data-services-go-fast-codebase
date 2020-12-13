24fa296ecfcd0e6b66c9b27c7f977aee7af3f6f4
Samuel Lee <lees@satalyst.com>
2020-12-13 19:40:44 +0800

    Added radio templates
    Renaming of CheckBoxOptionFor to InputSelectionOptionsFor to better reflect radio button enums
    
    Reworked some templates to display options into 2 cols

---
+++ solution/WebApplication/WebApplication/Models/Customisations/WizardReference/InputOption.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.Models.Customisations.WizardReference
+{
+    public class InputOption<U> where U : System.Enum
+    {
+        public U Option { get; set; }
+        public bool IsSelected { get; set; }
+
+    }
+}
---
+++ solution/WebApplication/WebApplication/Models/Customisations/WizardReference/InputSelectionOptionsFor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models.Customisations.WizardReference
+{
+  
+    public class InputSelectionOptionsFor<T> where T : System.Enum
+    {
+        public List<InputOption<T>> Options { get; set; } = new List<InputOption<T>>();
+
+        public InputSelectionOptionsFor()
+        {
+            foreach (T option in Enum.GetValues(typeof(T)))
+            {
+                Options.Add(new InputOption<T>
+                {
+                    Option = option,
+                    IsSelected = false
+                });
+            }
+        }
+
+        public string TemplateName
+        {
+            get
+            {                
+                var genericType = typeof(T).Name;
+                return $"EditorFor{genericType}";
+            }
+        }
+
+    }
+}
